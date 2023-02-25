4646d58de162870615f452ecbe624d6e9e47bdef
thanhtunggc92 <nguyentruongthanhtung@gmail.com>
2023-02-25 00:06:14 -0800

    create user app

--- devsearch/devsearch/urls.py
+++ devsearch/devsearch/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('projects.urls'))
+    path('',include('projects.urls')),
+    path('profiles',include('users.urls')),
 ]
 
 
--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def Profile(request):
+    context={}
+
+    return render(request,'users/profile.html',context)
