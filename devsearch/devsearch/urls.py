9795079db76d5fad0f52a1aecd393d6ea8bdb0dc
thanhtunggc92 <nguyentruongthanhtung@gmail.com>
2023-03-04 14:21:10 -0800

    complete send main and reset password function

--- devsearch/devsearch/urls.py
+++ devsearch/devsearch/urls.py
@@ -17,12 +17,29 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('projects.urls')),
     path('profiles/',include('users.urls')),
+    # path('reset_password',auth_views.PasswordResetView.as_view(), name='reset_password'),
+    # path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+    # path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name= 'password_reset_confirm'),
+    # path('resest_password_complete/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
+     path('reset_password/', auth_views.PasswordResetView.as_view(template_name='reset_password.html'),
+         name="reset_password"),
+
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='reset_password_sent.html'),
+         name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='reset.html'),
+         name="password_reset_confirm"),
+
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='reset_password_complete.html'),
+         name="password_reset_complete"),
 ]
+
 
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
